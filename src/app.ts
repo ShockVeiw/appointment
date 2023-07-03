7730da6662f519492a811f29641757d53d71947a
Leonid Fedorov <lfedorov887@gmail.com>
2023-07-03 19:07:15 +0300

    connect db

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import { connect } from 'mongoose';
 
 const app = express();
 const port = 8080;
 
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${ port }`);
-});
+connect('mongodb://localhost:27017/appointment')
+    .then(() => {
+        console.log('db connected');
+
+        app.listen(port, () => {
+            console.log(`server started at http://localhost:${ port }`);
+        });
+    })
+    .catch(err => {
+        console.error("Error during database connection:", err);
+    })
